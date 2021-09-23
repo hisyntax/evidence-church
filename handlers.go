6554944539ad4adfaa5f8263107ddf878ec02102
hisyntax <hisyntax0@gmail.com>
2021-09-23 14:03:45 +0100

    index.html completed

--- handlers.go
+++ handlers.go
@@ -8,6 +8,9 @@ import (
 var index = template.Must(template.ParseFiles("templates/index.html"))
 var sermon = template.Must(template.ParseFiles("templates/sermon.html"))
 
+// var contact = template.Must(template.ParseFiles("templates/contact.html"))
+// var about = template.Must(template.ParseFiles("templates/about.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	index.Execute(w, nil)
 }
